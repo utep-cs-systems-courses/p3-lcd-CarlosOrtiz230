264262ede038e80ee268abd0f919ffdbc10e9bc2
CarlosOrtiz230 <83482553+CarlosOrtiz230@users.noreply.github.com>
2023-07-31 19:24:52 -0600

    Delete myProgram directory

--- myProgram/draws.c
+++
@@ -1,9 +0,0 @@
-#include <msp430.h>
-#include <libTimer.h>
-#include "lcdutils.h"
-#include "lcddraw.h"
-
-void funnyFace()
-{
-  fillRectangle(30,30,60,60,COLOR_BLACK);
-}
--- myProgram/draws.h
+++
@@ -1,6 +0,0 @@
-#ifndef draws_included
-#define draws_included
-
-void funnyFace();
-
-#endif
