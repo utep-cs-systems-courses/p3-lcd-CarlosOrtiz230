cb86371fb4619f426bc852e20f679761bad36ec3
CarlosOrtiz230 <83482553+CarlosOrtiz230@users.noreply.github.com>
2023-08-02 08:54:22 -0600

    Update myDraws.c

--- program/myDraws.c
+++ program/myDraws.c
@@ -51,43 +51,7 @@ void funnyFace()
 
 }//method ends
 
-void funnyFace()
-{
-  int posRow = 20;
-  int posCol= 20;
-  int i;
-  //lentes
-  fillRectangle(20,30,30,30,COLOR_BLACK);
-  fillRectangle(80,30,30,30,COLOR_BLACK);
-  //conector
-  for(int i = 0;i<45;i++)
-  {
-    drawPixel(90+i,30,COLOR_BLACK);
-    drawPixel(30+i,30,COLOR_BLACK);
-    drawPixel(50+i,40,COLOR_BLACK);
-  }
-  //nouse
-  posCol = 40;
-  posRow = 90;
-  for(int i = 0; i<30; i++)
-  {
-    drawPixel(posCol+i,posRow-i,COLOR_ORANGE);
-    drawPixel(posCol+i,posRow,COLOR_ORANGE);
-  }
-  //mouth
-  posCol = 30;
-  posRow = 120;
-  for(int i = 0; i<70; i++)
-  {
-    drawPixel(posCol+i,posRow,COLOR_ORANGE);
-    drawPixel(posCol+i,posRow,COLOR_ORANGE);
-  }
 
-  fillRectangle(80,30,30,30,COLOR_BLACK);
-  fillRectangle(80,30,20,20,COLOR_RED);
-    
-
-}//method ends
 
 
 void dayHouse()
