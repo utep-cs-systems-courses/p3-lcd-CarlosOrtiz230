d3a7f6eff19df3e0b10ac9f9f206ef58c0704219
CarlosOrtiz230 <83482553+CarlosOrtiz230@users.noreply.github.com>
2023-08-02 09:14:29 -0600

    Update myDraws.h

--- program2/myDraws.h
+++ program2/myDraws.h
@@ -2,11 +2,12 @@
 #define _MY_DRAWS_H_
 #include <lcdutils.h>
 
-void funnyFace();
-void dayHouse();
-void drawLine(int x0, int y0, int x1, int y1, int color);
-int globalCounter();
-void mainScreen();
-void drawCow();
+typedef struct 
+{
+    int row, col,old_row,old_col;
+    u_char height;
+    u_char width;
+} Rectangle;
+
 
 #endif
