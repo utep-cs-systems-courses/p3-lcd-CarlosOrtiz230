ce72a6f04adaf28eeaf324ae3b189d62f2bf8ee8
CarlosOrtiz230 <83482553+CarlosOrtiz230@users.noreply.github.com>
2023-08-02 12:02:39 -0600

    Update myDraws.c

--- program2/myDraws.c
+++ program2/myDraws.c
@@ -34,9 +34,9 @@ void move(Rectangle* rect, char* dir)
   fillRectangle(rect->old_col - (rect->width / 2), rect->old_row - (rect->height / 2), rect->width, rect->height, COLOR_BLACK);
 
   // Update the rectangle's position based on the direction
-  if (direction == UP) {
+  if (direction == "U") {
     rect->row -= 10; // Move up by 5 pixels
-  } else if (direction == DOWN) {
+  } else if (direction == "D") {
     rect->row += 10; // Move down by 5 pixels
   }
 
