6a7be728ff2586bb81ef116b4bffcb3d674ac8f1
CarlosOrtiz230 <83482553+CarlosOrtiz230@users.noreply.github.com>
2023-08-02 11:05:11 -0600

    Update stateMachine.c

--- program2/stateMachine.c
+++ program2/stateMachine.c
@@ -9,17 +9,15 @@ void state_advance(int state){
       move(&rectangle,"U");
       break;
     case 2:
-       move((&rectangle1,"D");
+       move(&rectangle1,"D");
        break;
     case 3:
-      move((&rectangle1,"L");
+      move(&rectangle1,"L");
       break;
     case 4:
-      move((&rectangle1,"R");
-       break;
-      
-    default:
-      
+      move(&rectangle1,"R");
+       break;  
+    default;
       break;
   }
 }
